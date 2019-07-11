554e763e3bee7e9b7fb942dff5c2020b0934e498
Benjamin Sherman <benjamin@bensherman.io>
2019-07-11 01:00:07 -0400

    Change test naming scheme in test_echo to fix established pattens

--- bot/tests/test_triggers/test_commands/test_echo.py
+++ bot/tests/test_triggers/test_commands/test_echo.py
@@ -8,7 +8,7 @@ class TestEcho(unittest.TestCase):
         self.echo = echo.Echo()
 
     @test_utils.async_test
-    async def test_echo_with_content(self):
+    async def test_echo(self):
         test_strings = ["abcd", "hello world!", "הברווז אומר היי"]
         for string in test_strings:
             msg = test_utils.init_message(f"!echo {string}")
@@ -20,7 +20,7 @@ class TestEcho(unittest.TestCase):
             self.assertEqual(msg.channel.test_result, string)
 
     @test_utils.async_test
-    async def test_echo_without_content(self):
+    async def test_echo_empty(self):
         for num_spaces in range(0, 10):
             msg = test_utils.init_message("!echo" + " " * num_spaces)
             self.assertFalse(
