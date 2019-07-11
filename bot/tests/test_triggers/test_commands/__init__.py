3f5133a71e285401adf6139df85e3a39b828fa33
Benjamin Sherman <benjamin@bensherman.io>
2019-07-11 01:29:35 -0400

    Add man page tests

--- bot/tests/test_triggers/test_commands/__init__.py
+++ bot/tests/test_triggers/test_commands/__init__.py
@@ -3,5 +3,6 @@ from .test_code import TestCode
 from .test_echo import TestEcho
 from .test_latex import TestLatex
 from .test_lmdtfy import TestLmdtfy
+from .test_man import TestMan
 
-all_commands = [TestAI, TestCode, TestEcho, TestLatex, TestLmdtfy]
+all_commands = [TestAI, TestCode, TestEcho, TestLatex, TestLmdtfy, TestMan]
---
+++ bot/tests/test_triggers/test_commands/test_man.py
@@ -0,0 +1,35 @@
+import unittest
+from ... import test_utils
+from ....triggers.commands import man
+
+
+class TestMan(unittest.TestCase):
+    def setUp(self):
+        self.man = man.Man()
+
+    @test_utils.async_test
+    async def test_man_valid(self):
+        test_strings = [
+            ("https://linux.die.net/man/1/free", "free"),
+            ("https://linux.die.net/man/3/fgets", "fgets"),
+            ("https://linux.die.net/man/3/free", "3 free"),
+        ]
+        for string in test_strings:
+            msg = test_utils.init_message(f"!man {string[1]}")
+            self.assertTrue(
+                await self.man.execute(
+                    None, msg
+                )  # `client` is passed as None because it is never used
+            )
+            self.assertEqual(msg.channel.test_result, string[0])
+
+    @test_utils.async_test
+    async def test_man_empty(self):
+        for num_spaces in range(0, 10):
+            msg = test_utils.init_message("!man" + " " * num_spaces)
+            self.assertFalse(
+                await self.man.execute(
+                    None, msg
+                )  # `client` is passed as None because it is never used
+            )
+            self.assertIsNone(msg.channel.test_result)
