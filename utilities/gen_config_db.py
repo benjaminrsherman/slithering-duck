9be89799bd95ab5bd4354ddccc42d3d499307e4c
Benjamin Sherman <benjamin@bensherman.io>
2019-12-08 23:34:46 -0500

    Add class database update script

--- utilities/gen_config_db.py
+++ utilities/gen_config_db.py
@@ -7,6 +7,9 @@ if len(sys.argv) < 2 or not sys.argv[1].endswith(".db"):
     print(f"Usage: {sys.argv[0]} DB_FILE (JSON_FILE)")
     sys.exit(1)
 
+if os.path.exists(sys.argv[1]):
+    print("ERROR: database already exists... Aborting!")
+    sys.exit(1)
 
 connection = sqlite3.connect(sys.argv[1])
 c = connection.cursor()
@@ -19,7 +22,7 @@ c = connection.cursor()
 # identifiers is a json array as a string of the different abbreviations one might use for a class. EX: ["DS", "data struct"]
 c.execute(
     """
-    CREATE TABLE IF NOT EXISTS classes (
+    CREATE TABLE classes (
     ID INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     channel_id INTEGER DEFAULT 0,
@@ -33,7 +36,7 @@ c.execute(
 
 c.execute(
     """
-    CREATE TABLE IF NOT EXISTS emoji_users (
+    CREATE TABLE emoji_users (
     user_id INTEGER DEFAULT 0
     );
     """
@@ -41,34 +44,29 @@ c.execute(
 
 c.execute(
     """
-    CREATE TABLE IF NOT EXISTS emoji_channels (
+    CREATE TABLE emoji_channels (
     channel_id INTEGER DEFAULT 0
     );
     """
 )
 
-if len(sys.argv) > 2 and os.path.exists(sys.argv[2]):
+if len(sys.argv) > 2:
     print("Updating courses in database")
     with open(sys.argv[2], "r") as courses_file:
         courses = json.load(courses_file)
         for course in courses:
             # Discord limits the length of a channel to 100 characters, therefore we keep the course name as the first 100 chars in the database so we dont have to worry about that later
-            course_name_discord = course[:100]
+            course_name_discord = course[:100].upper()
 
-            c.execute(  # for a specific class gets rid of the old data so we can insert the new data from the json
-                "SELECT * FROM classes WHERE name = :name",
-                {"name": course_name_discord},
+            c.execute(  # inserts the new data for a class
+                """INSERT INTO classes (name, channel_id, course_codes, departments, identifiers) VALUES
+                   (:name, :channel_id, :course_codes, :departments, :identifiers );""",
+                {
+                    "name": course_name_discord,
+                    "channel_id": courses[course]["channel_id"],
+                    "course_codes": str(courses[course]["course_codes"]),
+                    "departments": str(courses[course]["departments"]),
+                    "identifiers": str(courses[course]["identifiers"]),
+                },
             )
-            if not c.fetchall():
-                c.execute(  # inserts the new data for a class
-                    """INSERT INTO classes (name, channel_id, course_codes, departments, identifiers) VALUES
-                       (:name, :channel_id, :course_codes, :departments, :identifiers );""",
-                    {
-                        "name": course_name_discord,
-                        "channel_id": courses[course]["channel_id"],
-                        "course_codes": str(courses[course]["course_codes"]),
-                        "departments": str(courses[course]["departments"]),
-                        "identifiers": str(courses[course]["identifiers"]),
-                    },
-                )
-                connection.commit()
+            connection.commit()
