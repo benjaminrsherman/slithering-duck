803530c9fe225a8a24ff511b2bbda64bd05c3360
Benjamin Sherman <benjamin@bensherman.io>
2019-12-09 00:00:25 -0500

    Resolve gitlab comments

--- bot/triggers/commands/delete.py
+++ bot/triggers/commands/delete.py
@@ -7,33 +7,44 @@ class Delete(Command, ReactionTrigger):
     prefixes = ["%"]
     requires_mod = True
 
-    async def prompt_for_channel_deletion(command_channel, channel_to_delete, author):
+    async def prompt_for_channel_deletion(
+        emoji, command_channel, channel_to_delete, author
+    ):
         await command_channel.send(
             f"WARNING: You are about to delete <#{channel_to_delete.id}>.  <@{author.id}> can confirm this action by reacting :+1: to this message."
-        ).add_reaction("👍")
+        ).add_reaction(emoji)
 
     async def execute_command(self, client, msg, content):
-        if not msg.author.guild_permissions.administrator:  # sorry joe
+        if not msg.author.guild_permissions.administrator:
             msg.channel.send(client.messages["invalid_permissions"])
             return
 
         if not msg.channel_mentions:
-            prompt_for_channel_deletion(msg.channel, msg.channel, msg.author)
+            prompt_for_channel_deletion(
+                client.get_emoji(653459984268656640),
+                msg.channel,
+                msg.channel,
+                msg.author,
+            )
             return
 
         for channel in msg.channel_mentions:
-            prompt_for_channel_deletion(msg.channel, channel, msg.author)
+            prompt_for_channel_deletion(
+                client.get_emoji(653459984268656640), msg.channel, channel, msg.author
+            )
 
     async def execute_reaction(self, client, reaction, channel, msg, user):
         if (
-            reaction.emoji != "👍"  # reaction must be a :+1:
+            reaction.emoji.id != 653459984268656640  # reaction must be a :+1:
             or not msg.author.bot  # must be reacting to a bot message
             or user not in msg.user_mentions  # prevent misclicks
             or not msg.content.startswith(
                 "WARNING: You are about to delete "
             )  # we must be deleting a channel
             or not msg.channel_mentions  # we need a channel to delete
-            or not any(react.me and react.emoji == "👍")  # the bot must agree to this
+            or not any(
+                react.me and react.emoji.id == 653459984268656640
+            )  # the bot must agree to this
         ):
             return
 
--- bot/triggers/commands/poll.py
+++ bot/triggers/commands/poll.py
@@ -11,5 +11,9 @@ class Poll(Command):
     needsContent = False
 
     async def execute_command(self, client, msg, content):
+        if not content:
+            async for message in msg.channel.history(limit=1, before=msg):
+                msg = message
+
         await msg.add_reaction("👍")
         await msg.add_reaction("👎")
