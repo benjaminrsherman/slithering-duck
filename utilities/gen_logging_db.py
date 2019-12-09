077d31b681d98d12fd4d318b092c0cfa1ca6f885
Benjamin Sherman <benjamin@bensherman.io>
2019-12-09 02:24:16 -0500

    Prevent gen_logging_db.py from running on an existing database

--- utilities/gen_logging_db.py
+++ utilities/gen_logging_db.py
@@ -7,13 +7,16 @@ if len(sys.argv) != 2 or not sys.argv[1].endswith(".db"):
     print(f"Usage: {sys.argv[0]} DB_FILE")
     sys.exit(1)
 
+if os.path.exists(sys.argv[1]):
+    print("ERROR: database already exists... Aborting!")
+    sys.exit(1)
 
 connection = sqlite3.connect(sys.argv[1])
 c = connection.cursor()
 
 c.execute(
     """
-    CREATE TABLE IF NOT EXISTS logging (
+    CREATE TABLE logging (
     source_channel_id INTEGER DEFAULT 0,
     dest_channel_id INTEGER DEFAULT 0
     );
@@ -22,7 +25,7 @@ c.execute(
 
 c.execute(
     """
-    CREATE TABLE IF NOT EXISTS unused_logging (
+    CREATE TABLE unused_logging (
     channel_id INTEGER DEFAULT 0
     );
     """
